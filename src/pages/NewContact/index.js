05cdb24dbc4798387857130a7f747e694f98d07b
Matheus <matheuscruzhen@gmail.com>
2022-09-21 16:12:58 -0300

    Criando os componentes Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
     <div>
       <PageHeader title="Novo contato" />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
     </div>
   );
 }
