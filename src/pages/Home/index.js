bdec77b9cb27196aa771dbe73828c836376981f8
Matheus <matheuscruzhen@gmail.com>
2022-09-21 15:31:05 -0300

    Criando a página Home e refatorando o Header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,88 @@
-import React from 'react';
+import {
+  Card,
+  Container,
+  Header,
+  InputSearchContainer,
+  ListContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+      </ListContainer>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Mateus Silva</strong>
+            <small>instagram</small>
+          </div>
+          <span>mateus@devacademy.com.br</span>
+          <span>(41) 99999-9999</span>
+        </div>
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="trash" />
+          </button>
+        </div>
+      </Card>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Mateus Silva</strong>
+            <small>instagram</small>
+          </div>
+          <span>mateus@devacademy.com.br</span>
+          <span>(41) 99999-9999</span>
+        </div>
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="trash" />
+          </button>
+        </div>
+      </Card>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Mateus Silva</strong>
+            <small>instagram</small>
+          </div>
+          <span>mateus@devacademy.com.br</span>
+          <span>(41) 99999-9999</span>
+        </div>
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="trash" />
+          </button>
+        </div>
+      </Card>
+    </Container>
+  );
 }
