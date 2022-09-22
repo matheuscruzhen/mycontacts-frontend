0414c6faf2176f235ce87296e85b5e9204e863e2
Matheus <matheuscruzhen@gmail.com>
2022-09-22 17:30:32 -0300

    Status de danger no Modal

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -7,6 +7,11 @@ export default {
       main: '#5061fc',
       dark: '#3346f0',
     },
+    danger: {
+      light: '#f97171',
+      main: '#fc5050',
+      dark: '#f63131',
+    },
     gray: {
       200: '#bcbcbc',
       900: '#222',
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Button from '../Button';
 
 import { Container, Footer, Overlay } from './styles';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Título do modal</h1>
         <p>Corpo do modal</p>
         <Footer>
           <button type="button" className="cancel-button">
             Cancelar
           </button>
-          <Button type="button">Deletar</Button>
+          <Button type="button" danger={danger}>
+            Deletar
+          </Button>
         </Footer>
       </Container>
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
