ebebd4c424949cb5a1e46e0f29162150c821c380
Matheus <matheuscruzhen@gmail.com>
2023-01-26 19:45:17 -0300

    Implementando o servico de contatos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,6 +15,7 @@ import trash from '../../assets/images/icons/trash.svg';
 import Loader from '../../components/Loader';
 
 import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -35,14 +36,9 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(
-          `http://localhost:3001/contacts?orderBy=${orderBy}`
-        );
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        await delay(500);
-
-        const json = await response.json();
-        setContacts(json);
+        setContacts(contactsList);
       } catch (error) {
         console.log(error);
       } finally {
