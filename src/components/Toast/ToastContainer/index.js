78f395d383358cac336b1b03b17ebb85e9ce0c1d
Matheus Cruz <matheuscruzhen@gmail.com>
2025-04-21 14:50:27 -0300

    Criando o componente ToastMessage

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,9 +1,12 @@
+import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
 export default function ToastContainer() {
   return (
     <Container>
-      <h1>Toast Container</h1>
+      <ToastMessage text="Default toast" />
+      <ToastMessage text="Error toast" type="danger" />
+      <ToastMessage text="Success toast" type="success" />
     </Container>
   );
 }
