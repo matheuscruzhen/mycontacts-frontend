fe9cc1e9214e2660c511afd6996856b3f1cd47b7
Matheus Cruz <matheuscruzhen@gmail.com>
2025-04-21 14:20:30 -0300

    Iniciando o desenvolvimento do componente de toasts

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,19 @@
 import { ThemeProvider } from 'styled-components';
-// import { BrowserRouter } from 'react-router-dom';
 
+import { Container } from './styles';
 import Routes from '../../routes';
-import Header from '../Header';
-
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
-import { Container } from './styles';
+
+import Header from '../Header';
+import ToastContainer from '../Toast/ToastContainer';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <ToastContainer />
       <GlobalStyles />
+
       <Container>
         <Header />
         <Routes />
