79e76f8dab1251c8dc553bc1af203d979feca1ba
Matheus Cruz <matheuscruzhen@gmail.com>
2025-04-24 17:45:52 -0300

    Limpando os dados do formulário após o envio

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -76,6 +76,10 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
       categoryId,
     });
     setIsSubmitting(false);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   }
 
   return (
