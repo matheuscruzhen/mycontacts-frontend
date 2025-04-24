16dedf55c9f4ff9568d387868d8b578d97cd8632
Matheus Cruz <matheuscruzhen@gmail.com>
2025-04-24 18:08:17 -0300

    Carregar os dados do contato na página de edição

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
+import toast from '../../utils/toast';
+
+import Loader from '../../components/Loader';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
-  return (
-    <div>
-      <PageHeader title="Editar Mateus Silva" />
+  const { id } = useParams();
+  const history = useHistory();
 
-      <ContactForm buttonLabel="Salvar alterações" />
-    </div>
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+      <div>
+        <PageHeader title="Editar Mateus Silva" />
+
+        <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
+      </div>
+    </>
   );
 }
