d988265c7d0781892f309d190f44387a08ab4bcc
Matheus Cruz <matheuscruzhen@gmail.com>
2025-05-06 00:12:35 -0300

    Implementando o container/presentational pattern

---
+++ src/pages/EditContact/container.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
+
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import Presentation from './presentation';
+
+export default function Container() {
+  const { id } = useParams();
+  const history = useHistory();
+  const contactFormRef = useRef(null);
+  const safeAsyncAction = useSafeAsyncAction();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setContactName(contact.name);
+          setIsLoading(false);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado!',
+          });
+        });
+      }
+    }
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  async function handleSubmit(contact) {
+    try {
+      const contactData = await ContactsService.updateContact(id, contact);
+      setContactName(contactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
+  return (
+    <Presentation
+      isLoading={isLoading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
