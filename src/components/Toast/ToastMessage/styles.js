e292e918c57d34af96fa957944b6d078102ef160
Matheus Cruz <matheuscruzhen@gmail.com>
2025-05-08 17:27:51 -0300

    Animando a entrada de componentes em listas

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,15 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const messageIn = keyframes`
+  from { 
+    opacity: 0; 
+    transform: translateY(100px);
+  }
+  to { 
+    opacity: 1; 
+    transform: translateY(0px);
+  }
+`;
 
 const containerVariants = {
   default: css`
@@ -22,6 +33,7 @@ export const Container = styled.div`
   justify-content: center;
   gap: 8px;
   cursor: pointer;
+  animation: ${messageIn} 0.3s;
 
   &:focus {
     background: purple;
